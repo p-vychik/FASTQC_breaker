c0bb11b78fe00b5d36262740454b45d9637ac682
Elena Grigoreva <72381640+lengrigo@users.noreply.github.com>
2021-11-18 21:24:28 +0700

    Update reports.py

--- reports.py
+++ reports.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
+
 # -----------------------------------------------------------------------------
 def make_report_basic_statistics(sequences, filename):
 
